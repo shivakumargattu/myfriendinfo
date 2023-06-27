b31e85e10da48a8eb86b0dd4d6ecfe4c75acad98
shiva kumar gattu <121631767+shivakumargattu@users.noreply.github.com>
2023-06-27 19:06:29 +0530

    from
    .

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./index.css"
-import {
-  FaMapMarkerAlt,
-  FaPhoneAlt,
-  FaEnvelope,
-  FaGlobe,
-  FaAngleDoubleRight
-} from "react-icons/fa";
+import {AiFillFileText} from "react-icons/ai"
 
 
 
 const ContactUs = () => {
 
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageUrl, setImageUrl] = useState('');
+  const [showImage, setShowImage] = useState(false);
+
   const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setShowImage(false);
+  };
+
+  const handleImageUrlChange = (event) => {
+    setImageUrl(event.target.value);
+    setShowImage(true);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission with the selected file or image URL
+    if (selectedFile) {
+      console.log('Selected file:', selectedFile);
+      // Add your logic here to process the file or send it to the server
+    } else if (imageUrl) {
+      console.log('Image URL:', imageUrl);
+      // Add your logic here to handle the image URL
+    }
+  };
+
+ /* const handleFileChange = (event) => {
     const file = event.target.files[0];
     console.log(file); // Do something with the uploaded file
-  };
+  }; */
 
   return (
     <>
@@ -65,9 +86,9 @@ const ContactUs = () => {
               
                  <div className='conatct-detali-items-container pt-0'>  
                  <img className='arow-imge-bottom' src=" https://img.webcodegenie.com/img/footer/sky.svg?ver=20230620061009" alt="emali"/>
-                 <p className='space-conat-info-itcons'>
+               <a href='https://myfriend.net.in/' className='active link-footers' > <p className='space-conat-info-itcons'>
                   info@myfriend.net.in
-                 </p>
+                 </p></a>
                  </div>
               
                  </div>
@@ -80,17 +101,38 @@ const ContactUs = () => {
          <form className="my-form">
         
         
-        <input type="text" className='input-flids' placeholder='Full Name*' required />
+        <input type="text" className='input-flids' placeholder='   Full Name*' required />
         <br/>
         
       
-        <input type="email" id="email" className='input-flids' placeholder="Email*"  name="email" required />
-        <br/>
-        <input type="file" id="file" className='input-flids' name="file" onChange={handleFileChange} />
-        <br/>
-      
-        <textarea id="message" name="message" placeholder='Message*' required></textarea><br/>
+        <input type="email" id="email" className='input-flids' placeholder="   Email*"  name="email" required />
 
+        <br/>
+        <input type="number" id="phone" className='input-flids' placeholder="   Phnoe Number*"  name="phone" required />
+     <br/>
+       
+        <textarea id="message" name="message" placeholder=' Message*' required></textarea>
+
+        <br/>
+        <label className='input-flids-label' htmlFor="fileInput">
+        {selectedFile ? (
+          <span>: {selectedFile.name}</span>
+        ) : (
+          <span className=''> <AiFillFileText/> Attach a file</span>
+        )}
+        <input
+          type="file"
+          id="fileInput"
+          onChange={handleFileChange}
+          accept=".txt, .pdf, .doc"
+          style={{ display: 'none' }}
+          placeholder='   Attach File'
+        />
+      </label>
+      <br/>
+       {/* <input type="file" id="file" className='input-flids' name="  file" onChange={handleFileChange} />
+        <br/>*/}
+      
         
         <button type="submit" className='get-in-btn nobg-btn'>Submit</button>
         </form> 
