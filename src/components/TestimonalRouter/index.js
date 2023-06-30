37463d93b4d328b958d026ecd3f7f796a4287231
shiva kumar gattu <121631767+shivakumargattu@users.noreply.github.com>
2023-06-30 09:23:48 +0530

    testimoels
    .

--- src/components/TestimonalRouter/index.js
+++ src/components/TestimonalRouter/index.js
@@ -1,44 +1,268 @@
-import React from 'react'
+import React,{useState} from 'react'
+import "./index.css"
+//*import {RxCross1} from "react-icons/rx"
 
 const TestimonalRouter = () => {
+ /* const [isRead,setRead]=useState(false)
+  const [cross,setCross]=useState(false)
+
+   const handelar=()=>{
+    setRead(true)
+   }
+   const wrongBtn=()=>{
+    setCross(setRead)
+   } */
+
   return (
     <>   
-     <div>
-      
-  
-
-     <div className='portfolio-cantainer'>
-             <h1 className='text'>Testimonials</h1> 
+     
+        <div className='portfolio-cantainer'>
+               <h1 className='text'>Testimonials</h1> 
              <h3 className='small-heading'>Appreciation from our Clients
-</h3>
-             <div className='portfolio-cards'>
+              </h3>
+              
+              
+               <div className='bg-tetinonal-container p-3'>
+
+
+                       <div class="flip-card-testimoneals m-3">
+                       <div class="flip-card-inner-testimoneals">
+                        <div class="flip-card-front-testimoneals">
+                      <div class="tectimoneal-card-front-end ">
+                     <img src="https://img.webcodegenie.com/img/testimonial/thumb-img.webp?ver=20230620061009" alt="John" className='w-100'/>
+                       <div className='text-left'>
+                      <h3 className='fontFamily'><b>SK Mamidipka</b></h3>
+                       <p class="title fontFamily"><b>CEO & Founder</b></p>
+                     <p className='fontFamily'><b>SK Creative Works</b></p>
+                  </div>
+                  </div>
+                    </div>
+                   <div class="flip-card-back-testimoneals">
+                     <h3 className='fontFamily'><b>SK Mamidipka</b></h3>
+                      <p className='fontFamily'><b>SK Creative Works</b></p>
+                     <p className='fontFamily'>
+					        	“The Web designing and Digital Marketing services are fabulous. WebIndia has been catering to my logo and poster needs for a long time and there were never any mishaps. Wonderful team.”					        </p>
+                  </div>
+               </div>
+              </div>
+
+             
+
+              <div class="flip-card-testimoneals m-3">
+                       <div class="flip-card--testimoneals">
+                        <div class="flip-card-front-testimoneals">
+                      <div class="tectimoneal-card-front-end ">
+                      <video src=' http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4' controls="controls" autoplay="false" className='w-100'/>
+                <div className='text-left'>
+                      <h3 className='fontFamily'><b>SK Mamidipka</b></h3>
+                       <p class="title fontFamily"><b>CEO & Founder</b></p>
+                     <p className='fontFamily'><b>SK Creative Works</b></p>
+                  </div>
+                  </div>
+                    </div>
+                {/*   <div class="flip-card-back-testimoneals">
+                     <h1 className='fontFamily'><b>SK Mamidipka</b></h1>
+                      <p className='fontFamily'><b>SK Creative Works</b></p>
+                     <p className='fontFamily'>
+					        	“The Web designing and Digital Marketing services are fabulous. WebIndia has been catering to my logo and poster needs for a long time and there were never any mishaps. Wonderful team.”					        </p>
+                  </div> */}
+               </div>
+              </div>
+
+
+
+          <div class="flip-card-testimoneals m-3">
+               <div class="flip-card-inner-testimoneals">
+                 <div class="flip-card-front-testimoneals">
+                   <div class="tectimoneal-card-front-end ">
+                     <img src="https://img.webcodegenie.com/img/testimonial/thumb-img.webp?ver=20230620061009" alt="John" className='w-100'/>
+                     <div className='text-left'>
+                      <h3 className='fontFamily'><b>Mahesh</b></h3>
+                      <p class="title fontFamily"><b>Manager of Company</b></p>
+                     <p className='fontFamily'><b>We love that guys</b></p>
+                   </div>
+                   </div>
+                 </div>
+               <div class="flip-card-back-testimoneals">
+                <h3 className='fontFamily'><b>Mahesh</b></h3>
+                 <p class="title fontFamily "><b>Manager of Company</b></p>
+                 <p>"Patience. Infinite patience. No shortcuts. Very well thought out and articulate communication. Clear milestones, deadlines and fast work. Even if the client is being careless. The best part...always solving problems with great original ideas!."					        </p>
+                </div>
+               </div>
+              </div>
+
+
+              
+
+         
+              <div class="flip-card-testimoneals m-3">
+                       <div class="flip-card--testimoneals">
+                        <div class="flip-card-front-testimoneals">
+                      <div class="tectimoneal-card-front-end ">
+                      <video src=' http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4' controls="controls" autoplay="false" className='w-100'/>
+                <div className='text-left'>
+                      <h3 className='fontFamily'><b>SK Mamidipka</b></h3>
+                       <p class="title fontFamily"><b>CEO & Founder</b></p>
+                     <p className='fontFamily'><b>SK Creative Works</b></p>
+                  </div>
+                  </div>
+                    </div>
+                {/*   <div class="flip-card-back-testimoneals">
+                     <h1 className='fontFamily'><b>SK Mamidipka</b></h1>
+                      <p className='fontFamily'><b>SK Creative Works</b></p>
+                     <p className='fontFamily'>
+					        	“The Web designing and Digital Marketing services are fabulous. WebIndia has been catering to my logo and poster needs for a long time and there were never any mishaps. Wonderful team.”					        </p>
+                  </div> */}
+               </div>
+              </div>
+
+
+          <div class="flip-card-testimoneals m-3">
+               <div class="flip-card-inner-testimoneals">
+                 <div class="flip-card-front-testimoneals">
+                   <div class="tectimoneal-card-front-end ">
+                     <img src="https://img.webcodegenie.com/img/testimonial/thumb-img.webp?ver=20230620061009" alt="John" className='w-100'/>
+                     <div className='text-left'>
+                      <h3 className='fontFamily'><b>Swetha</b></h3>
+                      <p class="title fontFamily"><b>Founder & CEO</b></p>
+                     <p className='fontFamily'><b>Meon IT Solutions</b></p>
+                   </div>
+                   </div>
+                 </div>
+               <div class="flip-card-back-testimoneals">
+                <h3 className='fontFamily'><b>Swetha</b></h3>
+                <p class="title fontFamily"><b>Founder & CEO</b></p>
+                 <p class="title fontFamily "><b>Meon IT Solutions</b></p>
+                 <p>  "Very well thought out and articulate communication. Clear milestones, deadlines and fast work. Patience. Infinite patience. No shortcuts. Even if the client is being careless. The best part...always solving problems with great original ideas!."					            </p>
+                </div>
+               </div>
+              </div> 
+
+
+
+          
+              <div class="flip-card-testimoneals m-3">
+                       <div class="flip-card--testimoneals">
+                        <div class="flip-card-front-testimoneals">
+                      <div class="tectimoneal-card-front-end ">
+                      <video src=' http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4' controls="controls" autoplay="false" className='w-100'/>
+                <div className='text-left'>
+                      <h3 className='fontFamily'><b>SK Mamidipka</b></h3>
+                       <p class="title fontFamily"><b>CEO & Founder</b></p>
+                     <p className='fontFamily'><b>SK Creative Works</b></p>
+                  </div>
+                  </div>
+                    </div>
+                {/*   <div class="flip-card-back-testimoneals">
+                     <h1 className='fontFamily'><b>SK Mamidipka</b></h1>
+                      <p className='fontFamily'><b>SK Creative Works</b></p>
+                     <p className='fontFamily'>
+					        	“The Web designing and Digital Marketing services are fabulous. WebIndia has been catering to my logo and poster needs for a long time and there were never any mishaps. Wonderful team.”					        </p>
+                  </div> */}
+               </div>
+              </div>
+               
+              
+          <div class="flip-card-testimoneals m-3">
+               <div class="flip-card-inner-testimoneals">
+                 <div class="flip-card-front-testimoneals">
+                   <div class="tectimoneal-card-front-end ">
+                     <img src="https://img.webcodegenie.com/img/testimonial/thumb-img.webp?ver=20230620061009" alt="John" className='w-100'/>
+                     <div className='text-left'>
+                      <h3 className='fontFamily'><b>Garrett</b></h3>
+                      <p class="title fontFamily"><b>CTO, IYBA</b></p>
+                     
+                   </div>
+                   </div>
+                 </div>
+               <div class="flip-card-back-testimoneals">
+                <h3 className='fontFamily'><b>Garrett</b></h3>
+                 <p class="title fontFamily "><b>CTO, IYBA</b></p>
+                 <p>"The My Friend Info Solutions team has been great to work with. They are reliable, knowledgeable and do whatever it takes to make a project successful. Whatever the task, they pull in the right people to make it happen, so you're never locked in to one person putting your long-term goals at risk. Thank you WCG team!
+
+</p>
+                </div>
+               </div>
+              </div>
+
+              <div class="flip-card-testimoneals m-3">
+               <div class="flip-card-inner-testimoneals">
+                 <div class="flip-card-front-testimoneals">
+                   <div class="tectimoneal-card-front-end ">
+                     <img src="https://img.webcodegenie.com/img/testimonial/thumb-img.webp?ver=20230620061009" alt="John" className='w-100'/>
+                     <div className='text-left'>
+                      <h3 className='fontFamily'><b>Soft Tech</b></h3>
+                      <p class="title fontFamily"><b>Manager of Company</b></p>
+                     
+                   </div>
+                   </div>
+                 </div>
+               <div class="flip-card-back-testimoneals">
+                <h3 className='fontFamily'><b>Soft Tech</b></h3>
+                 <p class="title fontFamily "><b>Manager of Company</b></p>
+                 <p>"Patience. Infinite patience. No shortcuts. Very well thought out and articulate communication. Clear milestones, deadlines and fast work. Even if the client is being careless. The best part...always solving problems with great original ideas!."					        </p>
+                </div>
+               </div>
+              </div>
+
+
+            </div>
+
+
+
+
+            {/* <div className='portfolio-cards'>
                
                 <div className="imge-proto-card  m-3 shadow">
 
-                <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" className='w-100'  controls="controls" autoplay="true" />     
-                   <div className="card-body m-3">
-                   <h5><b>App for Virtual Reality</b></h5>
-                    <p >DESIGN / IDEAS </p>
+                <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" className='w-100'  controls="controls"  />     
+                   <div className="text-left">
+                   <h5><b>Daniela Cordes</b></h5>
+                    <p >Owner, Real Estate Mgmt Company </p>
+                    <p>Play</p>
                      
                    </div>
                </div>
                
-
-              
-               <div className="imge-proto-card shadow  m-3">
+             
+             {isRead? <div className='imge-proto-card over-flow shadow '>
+             <div className='names-of-cleint'> <h3>Mahesh</h3> <button className='transpent' onClick={wrongBtn}><RxCross1 color='White' size={30}/></button></div>
+<p className='text-left  pt-1 pb-0 p-4'> This company rescued my app from a shocking experience with a different company. They helped me get back my code and restore it. They also made improvements, and working with them after what I had experienced was a delight. They were chasing me often, which I love! and they stuck to our agreement and quote for the work needed. They had good insights also to improve the app. I highly recommend this company!</p>
+           
+             </div>
+              : <div className="imge-proto-card shadow  m-3">
                    <img src="https://myfriend.net.in/wp-content/uploads/2019/11/project-details-2.jpg" className="card-img-top" alt="protofloi"/>
-                   <div className="card-body m-3">
-                   <h5><b>Mobile Coin View App</b></h5>
-                    <p>DEVELOPMENT</p>
-
-                   </div>
+                   <div className="text-left m-3">
+                   <h5><b>Mahesh</b></h5>
+                    <p>DEVELOPMent</p>
+                    <button onClick={handelar}>{isRead? "Close" : "Read"}</button>
+                    </div>
                </div>
-          
+          }
 
 
+
+             {isRead? <div className='imge-proto-card over-flow shadow '>
+             <div className='names-of-cleint'> <h3>Mahesh</h3> <button className='transpent' onClick={wrongBtn}><RxCross1 color='White' size={30}/></button></div>
+<p className='text-left  pt-1 pb-0 p-4'> This company rescued my app from a shocking experience with a different company. They helped me get back my code and restore it. They also made improvements, and working with them after what I had experienced was a delight. They were chasing me often, which I love! and they stuck to our agreement and quote for the work needed. They had good insights also to improve the app. I highly recommend this company!</p>
+           
+             </div>
+              : <div className="imge-proto-card shadow  m-3">
+                   <img src="https://myfriend.net.in/wp-content/uploads/2019/11/project-details-2.jpg" className="card-img-top" alt="protofloi"/>
+                   <div className="text-left m-3">
+                   <h5><b>Mahesh</b></h5>
+                    <p>DEVELOPMent</p>
+                    <button onClick={handelar}>{isRead? "Close" : "Read"}</button>
+                    </div>
+               </div>
+          }
+             
+               
+
+ {/*
                <div className="imge-proto-card shadow  m-3">
                <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" className='w-100'  controls="controls"  />     
-                <div className="card-body m-3">
+                <div className="text-left">
                    <h5><b>Analysis of Security</b></h5>
                     <p>IDEAS / TECHNOLOGY</p>
                    </div>
@@ -47,7 +271,7 @@ const TestimonalRouter = () => {
              
                <div className="imge-proto-card shadow  m-3">
                    <img src="https://myfriend.net.in/wp-content/uploads/2019/11/project-details-4.jpg" className="card-img-top" alt="protofloi"/>
-                   <div className="card-body m-3">
+                   <div className="text-left">
                    <h5><b>eCommerce Website</b></h5>
                     <p>DESIGN / IDEAS</p>
                    </div>
@@ -56,7 +280,7 @@ const TestimonalRouter = () => {
               
                <div className="imge-proto-card shadow  m-3">
                <video src=' http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4' controls="controls" autoplay="false" className='w-100'/>
-                   <div className="card-body m-3">
+                   <div className="text-left">
                    <h5><b>App for Health</b></h5>
                     <p >DEVELOPMENT</p>
                    </div>
@@ -67,27 +291,16 @@ const TestimonalRouter = () => {
 
                <div className="imge-proto-card shadow  m-3">
                    <img src="https://myfriend.net.in/wp-content/uploads/2019/11/project-details-8.jpg" className="card-img-top" alt="protofloi"/>
-                   <div className="card-body m-3">
+                   <div className="text-left">
                     
                     <h5><b>Social Media App</b></h5>
                     <p>DESIGN / TECHNOLOGY</p>
                    </div>
                </div>
           
-              
-               <div className="imge-proto-card shadow  m-3">
-               <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" className='w-100'  controls="controls" autoplay="true" />     
-                 <div className="card-body m-3">
-                    <h5><b>Your New Reality</b></h5>
-                    <p>DESIGN / TECHNOLOGY</p>
-                   </div>
-               </div>
-          
-
-
-          
+                   
            </div>
-        </div>
+        </div>*/}
 
     </div>
 
