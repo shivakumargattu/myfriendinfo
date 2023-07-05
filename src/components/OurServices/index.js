4e8a9840d0cfecae721f54bb3d3049a6e535dada
shiva kumar gattu <121631767+shivakumargattu@users.noreply.github.com>
2023-07-05 18:27:10 +0530

    header
    .updTE

--- src/components/OurServices/index.js
+++ src/components/OurServices/index.js
@@ -10,8 +10,11 @@ const OurServices = () => {
    
     <div className=' servies-card-cantener'>
     <div class="ourservice-card">
-    <img alt='imge' className='webdevlopment-banner' src='https://amigainformatics.com/wp-content/uploads/2019/09/Web-mobile-development-banner-06.jpg'/>
+    <div className='webdevlopment-banner'>
+    <img  className='w-100 h-100'  alt="imhe" src='https://amigainformatics.com/wp-content/uploads/2019/09/Web-mobile-development-banner-06.jpg'/>
   
+    </div>
+   
   <div className="container">
     <h4><b>Web Development</b></h4>
     <p>	Searching for the top web development.You've found it! Our business provides outstanding web solutions and excels in both fields. Our talented team is capable of handling any project, whether it involves redesigning an already-existing website or building a strong enterprise platform. We are renowned for designing eCommerce websites in addition to being experts in web development. With the aid of our custom web development services, we assist companies in creating a user-friendly online presence that promotes productivity and expansion.
@@ -22,7 +25,10 @@ const OurServices = () => {
   </div>
 
   <div class="ourservice-card">
-  <img className='webdevlopment-banner' src="https://img.freepik.com/free-vector/new-app-development-desktop_23-2148684987.jpg?w=740&t=st=1688450217~exp=1688450817~hmac=a2aa2ee13f2cfca4a4e445985a2778caa6dab38f92838243acf1b5e1a535cae7" alt="alt"/>
+  <div className='webdevlopment-banner'>
+  <img className='w-100' src="https://jbsoftware.ca/wp-content/uploads/web-design.jpg" alt="alt"/>
+ 
+  </div>
   <div className="container pt-3">
     <h4><b>Web Design</b></h4>
     <p>					     
@@ -30,7 +36,7 @@ const OurServices = () => {
 </div>
   </div>
   <div class="ourservice-card">
-  <img  className='webdevlopment-banner' src="https://img.freepik.com/free-vector/app-development-illustration_52683-47931.jpg?t=st=1688449990~exp=1688450590~hmac=16fc07395268360152f150b653ee3394021d99b1618258f7c6ce6c0a7f5000df" alt="imge"/>
+  <img  className='webdevlopment-banner' src="https://th.bing.com/th/id/R.e822fcc7d659db502c5cdff5c88687de?rik=gByBmwNLCNWLSQ&riu=http%3a%2f%2fventureaviator.com%2fwp-content%2fuploads%2f2017%2f11%2fshutterstock_713185614.jpg&ehk=1mju%2bVNwJtOn4N5oTxhSwFGs7k8fXXYdNZ2yRyPEUSI%3d&risl=&pid=ImgRaw&r=0" alt="imge"/>
   <div className="container pt-3">
     <h4><b>Mobile App Development</b></h4>
 <p>My Friend Info Solutions stands out from the competition as the top mobile app development firm.  We are aware that every company has its own unique requirements and goals. Because of this, our team of top-notch app developers is exceptional at comprehending client needs and goes above and beyond to provide top-notch mobile application development services. Our products are inexpensive, whether they are for Android or customised mobile apps. Furthermore, we have a track record of delivering projects on time.
@@ -41,7 +47,7 @@ const OurServices = () => {
   </div>
   </div>
   <div class="ourservice-card">
-  <img  className='webdevlopment-banner' src="https://img.freepik.com/free-vector/flat-design-cms-concept-illustrated_23-2148796510.jpg?w=900&t=st=1688450684~exp=1688451284~hmac=5933e696b1d731c815454267115a1bd75f28e9698f6d740cbe0e6133dc3d3590" alt="imge"/>
+  <img  className='webdevlopment-banner' src="https://www.esspl.com/wp-content/uploads/2020/01/automation-testing-company-thegem-blog-default.jpg" alt="imge"/>
   <div className="container pt-3">
     <h4><b>SoftWare Testing</b></h4>
     <p>Empowering your digital success with our expert software testing services - seamless performance, exceptional user experience,Software testing is a crucial process in the development of software applications to ensure their quality, functionality, and reliability. It involves evaluating and validating software components and systems to identify defects, bugs, and potential issues that may impact the user experience or the performance of the software. The primary goal of software testing is to uncover problems and provide feedback to developers.				        </p>
@@ -49,9 +55,9 @@ const OurServices = () => {
   </div>
 
   <div class="ourservice-card">
-  <img className='webdevlopment-banner' src="https://img.freepik.com/premium-vector/marketing_23-2148021283.jpg?size=626&ext=jpg&uid=R108671411&ga=GA1.2.1014522793.1685352100&semt=ais" alt="img"/>
+  <img className='webdevlopment-banner' src="https://thumbs.dreamstime.com/b/digital-marketing-new-startup-project-millennials-business-team-hands-work-financial-reports-laptop-digital-119372834.jpg" alt="img"/>
   <div className="container pt-3">
-  <h4>Digital Marketing</h4>
+  <h4><b>Digital Marketing</b></h4>
     <p>Search Engine Optimization (SEO): SEO focuses on optimizing websites and online content to improve visibility and organic search engine rankings. By employing techniques like keyword research, on-page optimization, and link building, businesses can increase their website's visibility in search engine results pages (SERPs), driving more organic traffic and attracting potential customers.
 </p>
       </div>
